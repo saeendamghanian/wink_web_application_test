aca7ab7e25b6870789906ac332d4666aed1643ff
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-02-26 12:28:45 +0100

    Updates

---
+++ precache-manifest.6a891c90e3bda81875501819bce309ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9111677849629f358f597cc227e6e5",
+    "url": "/wink_web_application_test/index.html"
+  },
+  {
+    "revision": "0434f6d035aa40b0e331",
+    "url": "/wink_web_application_test/static/css/main.3f6d921a.chunk.css"
+  },
+  {
+    "revision": "f659fabed6200d1614cb",
+    "url": "/wink_web_application_test/static/js/2.cbabea5d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/wink_web_application_test/static/js/2.cbabea5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0434f6d035aa40b0e331",
+    "url": "/wink_web_application_test/static/js/main.26229bec.chunk.js"
+  },
+  {
+    "revision": "22bd06b40cfd558a7327",
+    "url": "/wink_web_application_test/static/js/runtime-main.299b2bb1.js"
+  }
+]);
